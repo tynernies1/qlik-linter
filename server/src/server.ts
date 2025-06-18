4f5908897f9496947182961ae091c6de99654231
muehan <tdc@gmx.ch>
2025-06-18 21:09:47 +0200

    refactoring

--- server/src/server.ts
+++ server/src/server.ts
@@ -35,6 +35,7 @@ import { getUppercaseQuickfix } from './quickFix/keywordToUppercase';
 import { multilineCommentToken } from './semanicToken/multilineCommentToken';
 import { ITokenData } from './semanicToken/ITokenData';
 import { QlikLanguageServerSettings } from './configuration/QlikLanguageServerSettings';
+import { semanticTokenFinder } from './semanicToken/semanticTokenFinder';
 
 let qlikKeywords: string[] = [];
 
@@ -287,110 +288,10 @@ connection.onRequest("textDocument/semanticTokens/full", async (params) => {
 
 	const tokenData: ITokenData[] = [];
 
-	// === 1. Handle multi-line comments ===
-	tokenData.concat(...multilineCommentToken(text, document, lines));
+	tokenData.push(...multilineCommentToken(text, document, lines));
+	tokenData.push(...semanticTokenFinder(lines, qlikKeywords));
 
-
-	for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
-		const line = lines[lineIndex];
-		const matches: { index: number, length: number, tokenType: string }[] = [];
-
-		// Helper function to collect matches
-		const collectMatches = (regex: RegExp, tokenType: string) => {
-			let match;
-			while ((match = regex.exec(line)) !== null) {
-
-				try {
-					if (match.index >= 0) {
-						matches.push({
-							index: match.index,
-							length: tokenType == "function" ? match[0].length - 1 : match[0].length,
-							tokenType
-						});
-					}
-				} catch (ex) {
-					console.log('error in line: ' + line + ' with regex: ' + regex + ' and match: ' + match[0] + ' length: ' + match[0].length + ' and index: ' + match.index + ' with error: ' + ex);
-					throw ex;
-				}
-			}
-		};
-
-		// Collect matches for all token types
-
-		// Match keywords
-		//const keywordPattern = new RegExp(`\\b(${qlikKeywords.join('|')})\\b`, 'gi');
-		//collectMatches(keywordPattern, "keyword");
-		const keywordPattern = new RegExp(`\\b(${qlikKeywords.join('|')})\\b`, 'gi');
-		collectMatches(keywordPattern, "keyword");
-
-		// detect function, ignore IF and JOIN
-		collectMatches(/\b(?!IF|JOIN\b)([A-Z_#]+)\s*\(/gi, "function");
-
-		// Match functions that start with SUB
-		collectMatches(/\b(?<=\b(?:SUB)\s)([A-Z0-9_#]+)(?=\(|\r)/gi, "function");
-		collectMatches(/\b(?<=\b(?:CALL)\s)([A-Z0-9_#]+)\s*[\(|;]?/gi, "function");
-
-		// Match properties that start with @
-		collectMatches(/\@([0-9]*)/g, "property");
-
-		// Match variables that start with SET or LET
-		collectMatches(/\b(?<=\b(?:SET|LET)\s)[a-zA-Z_]*\.?([a-zA-Z0-9_]*)\b/gi, "variable");
-
-		// Match variables that start with $()
-		collectMatches(/(\$\([a-zA-Z0-9_.]*)\)/g, "variable");
-
-		// Match strings enclosed in single or double quotes
-		collectMatches(/["'](?:(?=(\\?)).)*?["']/g, "string");
-
-		// Match strings that start with AS and end with ], allowing for any content in between
-		collectMatches(/(?<=(?:AS)\s)[\["]{1}[a-zA-Z0-9_\-+%/\\&$# ]*[\]"]{1}/gi, "string");
-
-		// Match lib: URLs
-		// Match strings that start with lib: and end with ], allowing for any content in between
-		collectMatches(/\[lib:\/\/[^\]].*]/gi, "string");
-
-		// Single-line comments
-		// Match comments that start with //, but not those that start with lib:
-		collectMatches(/(?<!lib:)\/\/.*/g, "comment");
-
-		// Multi-line comments
-		// Match comments that start with /* and end with */, allowing for any content in between
-		collectMatches(/\/\*[\s\S]*?\*\//g, "comment");
-
-		// Match class names that start with lib: but not the keyword lib
-		collectMatches(/^\s*(?!lib$)([a-zA-Z0-9_]+:)/g, "class");
-		// Match class names after FROM, RESIDENT, and TABLE keywords, TABLE can have multiple classes separated by commas
-		collectMatches(/(?<=(?:FROM)\s)[\w]+/g, "class");
-		collectMatches(/(?<=(?:RESIDENT)\s)[\w]+/gi, "class");
-		collectMatches(/(?<=(?:STORE)\s)[\w]+/gi, "class");
-		collectMatches(/(?<=(?:TABLE)\s)[\w, ]+/gi, "class");
-		collectMatches(/(?<=(?:TO)\s)[\w,]+/gi, "class");
-
-		// Match parameters in function calls
-		collectMatches(/(?<=\(|,)\s*[^(),'"]+?\s*(?=,|\))/g, "parameter");
-
-		// Match decorators that start with trace
-		collectMatches(/(?<=(?:trace)\s)[a-z0-9 >:$(_)'.]*/gi, "decorator");
-
-		// Match operators
-		// find a solution for / not to be seen in paths and comments
-		collectMatches(/<>|<=|>=|==|=|<|>|\+|-|\*|%|&|\||!|~|<<|>>/g, "operator");
-
-		// Sort matches by index to ensure correct ordering
-		matches.sort((a, b) => a.index - b.index);
-
-		// Push tokens to builder
-		matches.forEach(({ index, length, tokenType }) => {
-			tokenData.push(
-				{
-					line: lineIndex,
-					startChar: index,
-					length: length,
-					tokenType: tokenType
-				});
-		});
-	}
-
+	// important to sort the tokens by line number, negative offset is not allowed
 	tokenData.sort((a, b) => a.line - b.line);
 
 	tokenData.forEach(({ line, startChar, length, tokenType }) => {
@@ -398,7 +299,7 @@ connection.onRequest("textDocument/semanticTokens/full", async (params) => {
 	});
 
 	const result = builder.build();
-	//console.log("[Server] Sending tokens:", result);
+
 	return result;
 });
 
